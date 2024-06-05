454b3860a503e7e43f821b6d4581eb5be067cf06
hector guerra <cabeuno@hotmail.com>
2024-06-04 19:33:59 -0500

    implementacion de side var y cambio a primeNG

--- src/app/screens/categoria/categoria.component.ts
+++ src/app/screens/categoria/categoria.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
-import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputSwitchModule } from 'primeng/inputswitch';
 
 @Component({
   selector: 'app-categoria',
   templateUrl: './categoria.component.html',
   styleUrls: ['./categoria.component.css'], standalone: true,
-  imports: [FormsModule, InputTextModule, FloatLabelModule, InputSwitchModule]
+  imports: [FormsModule, InputTextModule, InputSwitchModule]
   
 })
 export class CategoriaComponent {
